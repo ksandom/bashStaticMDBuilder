0f994dd5f1b415f723ea4e2b279d712930c537be
Kevin Sandom <kjsandom@gmail.com>
2020-08-10 19:10:15 +0200

    Shellcheck fixes

---
+++ config/config.example.sh
@@ -0,0 +1,22 @@
+# The essentials.
+domain="www.example.com"
+siteURL="http://$domain"
+bucket="s3://$domain"
+
+# The basics.
+siteTitle="Example Site"
+siteDescription="This is such a cool site that you should totally spend some time on."
+
+# How many recent posts to include in the related videos on the root page.
+howManyPostsOnRoot=3
+
+# Images.
+thumbnailSize=320X90
+previewMaxSize=1024
+useableMaxSize=1920
+
+# Recommended items.
+previewLength=30
+
+# Testing
+testPort=8000
