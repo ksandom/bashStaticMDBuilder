5abd06f062bc4261b470a053724603fe88c3265e
Kevin Sandom <kjsandom@gmail.com>
2024-08-30 22:23:55 +0200

    Improve video formatting control

--- bin/lib/videos.sh
+++ bin/lib/videos.sh
@@ -7,10 +7,11 @@ function videos
     while IFS= read -r line; do
         if [ "${line::29}" == "https://www.youtube.com/watch" ] || [ "${line::28}" == "http://www.youtube.com/watch" ] || [ "${line::17}" == "https://youtu.be/" ]; then
             id="$(echo "$line" | sed 's/^.*\(v=\|\.be\/\)//g;s/&.*$//g')"
-            echo '<iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/'"$id"'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+            # width="560" height="315"
+            echo '<DIV name=\"videoContainer\"><iframe class="video" src="https://www.youtube.com/embed/'"$id"'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></DIV>'
         elif [ "${line::18}" == 'https://vimeo.com/' ]; then
             id="$(echo "$line" | cut -d/ -f4)"
-            echo '<iframe src="https://player.vimeo.com/video/'"$id"'" class="video" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>'
+            echo '<DIV name=\"videoContainer\"><iframe src="https://player.vimeo.com/video/'"$id"'" class="video" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></DIV>'
         else
             echo "$line"
         fi
