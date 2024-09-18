c112653bf02de6027ca3730ff567d7d48c75331d
Kevin Sandom <kjsandom@gmail.com>
2024-09-18 17:29:16 +0200

    Support youtube live streams

--- bin/lib/videos.sh
+++ bin/lib/videos.sh
@@ -7,7 +7,9 @@ function videos
     while IFS= read -r line; do
         if [ "${line::29}" == "https://www.youtube.com/watch" ] || [ "${line::28}" == "http://www.youtube.com/watch" ] || [ "${line::17}" == "https://youtu.be/" ]; then
             id="$(echo "$line" | sed 's/^.*\(v=\|\.be\/\)//g;s/&.*$//g')"
-            # width="560" height="315"
+            echo '<DIV name=\"videoContainer\"><iframe class="video" src="https://www.youtube.com/embed/'"$id"'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></DIV>'
+        elif [ "${line::28}" == "https://www.youtube.com/live" ] || [ "${line::24}" == "https://youtube.com/live" ]; then
+            id="$(echo "$line" | sed 's/^.*\/live//g;s/?.*$//g')"
             echo '<DIV name=\"videoContainer\"><iframe class="video" src="https://www.youtube.com/embed/'"$id"'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></DIV>'
         elif [ "${line::18}" == 'https://vimeo.com/' ]; then
             id="$(echo "$line" | cut -d/ -f4)"
